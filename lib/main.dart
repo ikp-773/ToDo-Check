e3c9311f41464003dc3577ac37fef843053bebed
darklord773 <57410559+darklord773@users.noreply.github.com>
2020-08-02 18:44:27 +0530

    modified

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 void main() {
   runApp(MyApp());
 }
-
+//
 //FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
 //    FlutterLocalNotificationsPlugin();
 //
